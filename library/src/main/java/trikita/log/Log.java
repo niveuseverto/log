7988e4d99ded855c03f96fcf52885358774a6cb5
Alexander Filippov <a.filippov@technodeep.net>
2016-01-18 16:48:02 +0300

    Added constants to maintain backward compatibility

--- library/src/main/java/trikita/log/Log.java
+++ library/src/main/java/trikita/log/Log.java
@@ -21,6 +21,36 @@ public final class Log {
 	public final static int W = 3;
 	public final static int E = 4;
 
+	/**
+	 * Priority constant for the println method; use Log.v.
+	 */
+	public static final int VERBOSE = 2;
+
+	/**
+	 * Priority constant for the println method; use Log.d.
+	 */
+	public static final int DEBUG = 3;
+
+	/**
+	 * Priority constant for the println method; use Log.i.
+	 */
+	public static final int INFO = 4;
+
+	/**
+	 * Priority constant for the println method; use Log.w.
+	 */
+	public static final int WARN = 5;
+
+	/**
+	 * Priority constant for the println method; use Log.e.
+	 */
+	public static final int ERROR = 6;
+
+	/**
+	 * Priority constant for the println method.
+	 */
+	public static final int ASSERT = 7;
+
 	private Log() {}
 
 	public interface Printer {
@@ -235,7 +265,7 @@ public final class Log {
 					}
 				} catch (NoSuchFieldException|IllegalAccessException|
 						IllegalStateException|NullPointerException e) {
-					 //Ignore 
+					 //Ignore
 				}
 			}
 		} catch (ClassNotFoundException e) { /* Ignore */ }
